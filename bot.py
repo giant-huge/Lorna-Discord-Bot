5f3bd495004240412a0bbf2f7a973e3969c29986
andreasvondyvor <dyvormusic@gmail.com>
2021-02-05 14:36:28 -0500

    commit v8, music commands removed due to bugs.

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import youtube_dl
 
 import os
 import requests
@@ -54,68 +53,6 @@ async def on_message(message):
         await message.channel.send('I am here my liege, what you do need assistance with?')
         await message.channel.send('Do ^help for a list of commands.')
     await bot.process_commands(message)
-
-
-@bot.command()
-async def play(ctx, url : str):
-    song_there = os.path.isfile('song.mp3')
-    try:
-        if song_there:
-            os.remove('song.mp3')
-    except PermissionError:
-        await ctx.send('Wait your turn love.')
-        return
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Music Bot VC')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guilds=ctx.guild)
-
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-            }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.downloadn([url])
-    for file in os.listdir('./'):
-        if file.endswith('.mp3'):
-            os.rename(file, 'song.mp3')
-    voice.play(discord.FFmpegPCMAudio('song.mp3'))
-
-
-
-@bot.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guilds=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send('Love, I am not in the Music channel.')
-
-@bot.command()
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guilds=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send('No tunes jammin right now love.')
-
-@bot.command()
-async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guilds=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-    else:
-        await ctx.send('Me stereo is not paused.')
-
-@bot.command()
-async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guilds=ctx.guild)
-    voice.stop()
-
 
 
 

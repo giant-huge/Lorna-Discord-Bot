b10ff0445f89176feac9a7e1f28c4a726291afe4
andreasvondyvor <dyvormusic@gmail.com>
2021-02-12 20:45:09 -0500

    working 1

--- bot.py
+++ bot.py
@@ -1,13 +1,7 @@
 import discord
 from discord.ext import commands
 
-import os
 import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-WEATHER_TOKEN = os.getenv('WEATHER_TOKEN')
 
 bot = commands.Bot(command_prefix='^')
 
